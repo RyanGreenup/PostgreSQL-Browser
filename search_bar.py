b6fec444433b3a6338b281556c7916b6927c54d5
ryan <exogenesis@protonmail.com>
2024-10-14 21:47:31 +1100

    feat: Add search functionality to the search bar

--- search_bar.py
+++ search_bar.py
@@ -2,35 +2,52 @@ from PyQt6.QtWidgets import QApplication, QComboBox, QHBoxLayout, QLineEdit, QWi
 import sys
 from database_manager import DatabaseManager
 from warning_types import issue_warning, DatabaseWarning
+from gui_components import DBTablesTree
 
 
 class SearchWidget(QWidget):
-    def __init__(self, table: str, db_manager: DatabaseManager):
+    def __init__(self, db_manager: DatabaseManager, db_tree: DBTablesTree):
         super().__init__()
+
         self.db_manager = db_manager
-        self.table = table
+
+        # Get the Tree
+        self.db_tree = db_tree
+        self.db_tree.itemSelectionChanged.connect(self.populate_field_combo_box)
+        self.db_tree.itemSelectionChanged.connect(self.set_combo_box_selection)
+
+        # Search Bar
+        self.search_bar = QLineEdit()
+        self.search_bar.setPlaceholderText("Search for a term")
+        self.search_bar.returnPressed.connect(self.search_db)
+
         self.setup_ui()
 
     def setup_ui(self):
         layout = QHBoxLayout()
-        self.search_bar = QLineEdit()
-        self.search_bar.setPlaceholderText("Search for a term")
         self.field_combo_box = QComboBox()
         self.field_combo_box.setPlaceholderText("Select a field")
         self.populate_field_combo_box()
-        
+
         layout.addWidget(self.search_bar)
         layout.addWidget(self.field_combo_box)
         self.setLayout(layout)
 
+    def set_combo_box_selection(self):
+        current_table = self.db_tree.get_selected_table()
+        self.field_combo_box.setCurrentText(current_table)
+
+
     def populate_field_combo_box(self) -> bool:
         self.field_combo_box.clear()
         try:
-            databases = self.db_manager.list_databases()
-            self.field_combo_box.addItems(databases)
+            # TODO should I use the widget or the manager?
+            db_name = self.db_manager.current_database
+            tables = self.db_manager.get_tables(db_name)
+            self.field_combo_box.addItems(tables)
             # Select the first item
             self.field_combo_box.setCurrentIndex(0)
-            return len(databases) > 0
+            return len(tables) > 0
         except Exception as e:
             issue_warning("Error listing databases", DatabaseWarning, e)
             return False
@@ -43,8 +60,9 @@ class SearchWidget(QWidget):
     def search_db(self):
         search_term = self.search_bar.text()
         field = self.field_combo_box.currentText()
+        table = self.db_tree.get_selected_table()
         query = f"""
-        SELECT * FROM {self.table}
+        SELECT * FROM {table}
         WHERE {field} LIKE %s;
         """
         search_term = "%" + search_term + "%"
